975f79b945a661f190df188376183f0c3ca09640
Aaradhy Patil <213276136+Proxy939@users.noreply.github.com>
2026-01-15 13:47:33 +0530

    package update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\college\Projects\Kizuna (絆)\venv
+set VIRTUAL_ENV=D:\college\Projects\Kizuna Anime 4K Upscaler\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
